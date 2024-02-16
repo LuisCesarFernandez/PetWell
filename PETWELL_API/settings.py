4fe9f56f73ee3387bc372e8e4c158dd48be1ae6c
LuisCesar <luiscesarfernandez04@gmail.com>
2024-02-15 22:42:47 -0500

    conexión a la base de datos

--- PETWELL_API/settings.py
+++ PETWELL_API/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
+    'backpetwell'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'PETWELL_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'petwell',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': 3307,
     }
 }
 
